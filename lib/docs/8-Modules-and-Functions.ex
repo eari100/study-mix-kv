f98e62391cd5f3e1d835136aba2abebfcc87610d
Jaewook <eari100@naver.com>
2023-09-02 01:15:30 +0900

    기본 인수가 불필요하다면 warning

--- lib/docs/8-Modules-and-Functions.ex
+++ lib/docs/8-Modules-and-Functions.ex
@@ -1,6 +1,6 @@
 defmodule Math do
   def sum(a, b) do
-    a + b
+    do_sum(a, b)
   end
 
   defp do_sum(a, b) do
@@ -11,11 +11,11 @@ defmodule Math do
   def zero?(x) when is_integer(x), do: false
 end
 
-defmodule Concat do
-  def join(a, b, sep \\ " ") do
-    a <> sep <> b
-  end
-end
+#defmodule Concat do
+#  def join(a, b, sep \\ " ") do
+#    a <> sep <> b
+#  end
+#end
 
 defmodule DefaultTest do
   def dowork(x \\ "hello") do
@@ -23,14 +23,27 @@ defmodule DefaultTest do
   end
 end
 
+#defmodule Concat do
+#  def join(a, b \\ nil, sep \\ " ")
+#
+#  def join(a, b, _seq) when is_nil(b) do
+#    a
+#  end
+#
+#  def join(a, b, sep) do
+#    a <> sep <> b
+#  end
+#end
+
 defmodule Concat do
-  def join(a, b \\ nil, sep \\ " ")
-
-  def join(a, b, _seq) when is_nil(b) do
-    a
+  def join(a, b) do
+    IO.puts "***First join"
+    a <> b
   end
 
-  def join(a, b, sep) do
-    a <> sep <> b
-  end
+#  warning
+#  def join(a, b, sep \\ " ") do
+#    IO.puts "***Second join"
+#    a <> sep <> b
+#  end
 end
