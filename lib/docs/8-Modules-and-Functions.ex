197633c2c4c5e11181d67db2031acccec1415da6
Jaewook <eari100@naver.com>
2023-09-02 00:52:25 +0900

    zero? 함수 한 줄로 변경

--- lib/docs/8-Modules-and-Functions.ex
+++ lib/docs/8-Modules-and-Functions.ex
@@ -7,11 +7,6 @@ defmodule Math do
     a + b
   end
 
-  def zero?(0) do
-    true
-  end
-
-  def zero?(x) when is_integer(x) do
-    false
-  end
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
 end
