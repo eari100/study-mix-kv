e0aaaa24a8d1b877867a60a3f48ad8ececb475b1
Jaewook <eari100@naver.com>
2023-09-02 09:33:19 +0900

    recursion 를 이용하여 list 데이터들 합쳐서 반환하기

--- lib/docs/9-Recursion.ex
+++ lib/docs/9-Recursion.ex
@@ -8,3 +8,13 @@ defmodule Recursion do
     :ok
   end
 end
+
+defmodule Math do
+  def sum_list([head | tail], accumulator) do
+    sum_list(tail, head + accumulator)
+  end
+
+  def sum_list([], accumulator) do
+    accumulator
+  end
+end
