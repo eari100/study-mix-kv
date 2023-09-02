6acbf6dc53a5bd9a2a00f951c5551519a4b9edb6
Jaewook <eari100@naver.com>
2023-09-02 15:33:32 +0900

    열거형 타입, .., capture

---
+++ lib/docs/10-Enumerables-and-Streams.ex
@@ -0,0 +1,19 @@
+defmodule Study_Enumerables do
+  def sum(list) when(is_list(list)) do
+    Enum.map(list, fn x -> x * 2  end)
+  end
+
+  def sum(map) when(is_map(map)) do
+    Enum.map(%{1 => 2, 3 => 4}, fn {k, v} -> k * v end)
+  end
+
+  # ..
+  def sum_list() do
+    Enum.map(1..3, fn x -> x * 2 end)
+  end
+
+  # capture
+  def capture() do
+    Enum.reduce(1..3, &+/2)
+  end
+end
