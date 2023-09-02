79a2db6289254939ffca2ccc0bf9bb74d6a0b2d7
Jaewook <eari100@naver.com>
2023-09-02 14:44:13 +0900

    recursion 를 이용하여 list 데이터들 2배로 만들어서 반환

--- lib/docs/9-Recursion.ex
+++ lib/docs/9-Recursion.ex
@@ -18,3 +18,13 @@ defmodule Math do
     accumulator
   end
 end
+
+defmodule Math do
+  def double_each([head | tail]) do
+    [head * 2 | double_each(tail)]
+  end
+
+  def double_each([]) do
+    []
+  end
+end
